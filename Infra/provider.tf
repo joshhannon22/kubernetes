237249a315499d8e18fe4b4d7cd12d5b9faf6617
Josh Hannon <jhannon@westmonroepartners.com>
2025-03-19 14:40:51 -0400

    fixed resource group name

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = var.resource_group_name
+    resource_group_name   = "aks-resources"
     storage_account_name  = "joshaksbackend"
     container_name        = "tf-container"
     key                   = "terraform.tfstate"  # This is the name of the state file
