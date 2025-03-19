ee7f319e0e455f813d75189c655768f6b6e0cbe0
Josh Hannon <jhannon@westmonroepartners.com>
2025-03-19 14:38:03 -0400

    remote backend

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = "joshaksbackend"
+    container_name        = "tf-container"
+    key                   = "terraform.tfstate"  # This is the name of the state file
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
